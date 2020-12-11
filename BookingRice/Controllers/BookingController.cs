a35d5ba0ba4705fb314496b950e5d4afc64d697c
J.C <jeter.chen@frog-jump.com>
2020-12-11 22:42:07 +0800

    加入專案檔案。

---
+++ BookingRice/Controllers/BookingController.cs
@@ -0,0 +1,128 @@
+﻿using BookingRice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace BookingRice.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking/Index
+        public ActionResult Index()
+        {
+            var Menus = GetMenu();
+            return View(Menus);
+        }
+
+        // GET: Booking/StringPage 回傳 String
+        public String StringPage()
+        {
+            return "Booking Page";
+        }
+
+
+        // GET: Booking/Categorys 回傳 Json
+        public JsonResult Categorys()
+        {
+            var categorys = new object[]
+            {
+                new
+                {
+                    Name = "便當"
+                },
+                new
+                {
+                    Name = "炒飯"
+                },
+                new
+                {
+                    Name = "燴飯"
+                },
+                new
+                {
+                    Name = "炒麵"
+                },
+                new
+                {
+                    Name = "湯麵"
+                },
+                new
+                {
+                    Name = "小菜"
+                }
+            };
+            return Json(categorys, JsonRequestBehavior.AllowGet);
+        }
+
+        private List<RiceMenu> GetMenu()
+        {
+            List<RiceMenu> Menus = new List<RiceMenu>
+            {
+                new RiceMenu
+                {
+                    Category = "便當",
+                    Name = "雞腿飯",
+                    Price = 100
+                },
+                new RiceMenu
+                {
+                    Category = "便當",
+                    Name = "排骨飯",
+                    Price = 90
+                },
+                new RiceMenu
+                {
+                    Category = "便當",
+                    Name = "蝦排飯",
+                    Price = 90
+                },
+                new RiceMenu
+                {
+                    Category = "炒飯",
+                    Name = "肉絲炒飯",
+                    Price = 60
+                },
+                new RiceMenu
+                {
+                    Category = "炒飯",
+                    Name = "火腿炒飯",
+                    Price = 60
+                },
+                new RiceMenu
+                {
+                    Category = "炒飯",
+                    Name = "牛肉炒飯",
+                    Price = 90
+                },
+                new RiceMenu
+                {
+                    Category = "炒飯",
+                    Name = "蝦仁炒飯",
+                    Price = 80
+                },
+                 new RiceMenu
+                {
+                    Category = "湯麵",
+                    Name = "切仔麵",
+                    Price = 60
+                },
+                  new RiceMenu
+                {
+                    Category = "湯麵",
+                    Name = "烏龍麵",
+                    Price = 70
+                },
+                    new RiceMenu
+                {
+                    Category = "湯麵",
+                    Name = "乾麵",
+                    Price = 30
+                }
+            };
+            return Menus;
+        }
+    }
+}
---
+++ BookingRice/Models/RiceMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingRice.Models
+{
+    public class RiceMenu
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
